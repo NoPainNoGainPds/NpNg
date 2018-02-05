01d1b9d9dd5d3cbfc7d5e95b3915a470ae01dfc5
Ackincolor <lois.guillet@gmail.com>
2018-02-05 17:58:02 +0100

    debut fenetre

--- Prototype/src/controller/Main.java
+++ Prototype/src/controller/Main.java
@@ -6,7 +6,7 @@ import vue.Fenetre;
 public class Main{
     public static void main(String[] args)
     {
-        Fenetre fen = new Fenetre("Prototype");
+        Fenetre fen = new Fenetre("Prototype Lois");
         javax.swing.SwingUtilities.invokeLater(fen);
     }
 }
