def0a333f99978a617711cd80ed532ea57cdadf1
Ackincolor <lois.guillet@gmail.com>
2018-03-09 07:29:07 +0100

    modif GridBagLayout

--- Prototype/src/vue/RightPanelView.java
+++ Prototype/src/vue/RightPanelView.java
@@ -6,14 +6,41 @@ import utils.Constants;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ *
+ */
 public class RightPanelView extends JPanel {
+    /**
+     *
+     */
     private Boutique boutique;
+    /**
+     *
+     */
     private CardLayout cl;
+    /**
+     *
+     */
     private JPanel view1;
+    /**
+     *
+     */
     private JPanel view2;
+    /**
+     *
+     */
     private JLabel nomBoutique ;
+    /**
+     *
+     */
     private JLabel categorieEmplacement;
+    /**
+     *
+     */
     private JLabel nomEmplacement;
+    /**
+     *
+     */
     public RightPanelView()
     {
         this.cl = new CardLayout();
@@ -22,44 +49,72 @@ public class RightPanelView extends JPanel {
         this.buildFirstView();
         this.buildSecondView();
     }
+
+    /**
+     *
+     */
     private void buildFirstView()
     {
         this.view1 = new JPanel();
         GridBagLayout gbl = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
-        this.setLayout(gbl);
+        this.view1.setLayout(gbl);
         //this.view1.setLayout(new GridLayout(6,1));
         //add Label Boutique
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0.0;
+        gbc.weighty = 0.1;
         this.nomBoutique = new JLabel();
         this.nomBoutique.setPreferredSize(new Dimension(110,30));
+        this.view1.add(this.nomBoutique,gbc);
         //---------------
         //add Label location
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weightx = 0.0;
+        gbc.weighty = 0.2;
         this.nomEmplacement = new JLabel();
+        this.view1.add(this.nomEmplacement);
         //----------------
         //add label Categorie location
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.weightx = 0.0;
+        gbc.weighty = 0.2;
         this.categorieEmplacement = new JLabel();
+        this.view1.add(this.categorieEmplacement);
         //------------------------
-
+        //add "NEXT" btn
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.weightx = 0.0;
+        gbc.weighty = 0.1;
         JButton btn1 = new JButton("next");
         btn1.setMaximumSize(new Dimension(Constants.BTN_WIDTH,Constants.BTN_HEIGHT));
         btn1.addActionListener(event ->
         {
             this.change();
         });
-
-        this.view1.add(this.nomBoutique);
-        this.view1.add(this.nomEmplacement);
-        this.view1.add(this.categorieEmplacement);
-
         this.view1.add(btn1);
-
+        //----------------------
+        //add panel to right main panel
         this.add(this.view1,"view1");
     }
+
+    /**
+     *
+     */
     private void buildSecondView()
     {
         this.view2 = new JPanel();
         this.add(this.view2,"view2");
     }
+
+    /**
+     *
+     * @param b
+     */
     public void recieveData(Boutique b)
     {
         this.boutique = b;
@@ -69,6 +124,10 @@ public class RightPanelView extends JPanel {
         this.repaint();
         this.cl.show(this,"view1");
     }
+
+    /**
+     *
+     */
     public void change()
     {
         this.cl.next(this);
