21515a85fd0c4e6051215c42f7377bad0b72233b
Ackincolor <lois.guillet@gmail.com>
2018-03-21 22:06:17 +0100

    modif view

--- Prototype/src/vue/DetailStore.java
+++ Prototype/src/vue/DetailStore.java
@@ -22,6 +22,7 @@ import javax.swing.SwingUtilities;
 import model.Boutique;
 import model.Produit;
 import net.miginfocom.swing.MigLayout;
+import utils.Constants;
 import utils.MyListModel;
 import utils.daoUtils.ProduitDAO;
 
@@ -34,16 +35,16 @@ public class DetailStore extends JFrame implements Runnable {
 
     public DetailStore(Boutique store) {
         super("Detail Store");
-        this.setSize(new Dimension(500, 420));
+        this.setSize(new Dimension(500, 520));
         this.store = store;
         JPanel panel = new JPanel();
-        panel.setLayout(new MigLayout("inset 10", "[fill, grow]", "[fill, grow]"));
+        panel.setLayout(new MigLayout("inset 5", "[fill, grow]", "[fill, grow]"));
         URL url = null;
         BufferedImage c = null;
         this.img = Toolkit.getDefaultToolkit().getImage(this.store.getLogo());
         this.logo = new ImageComponent(true);
         this.logo.setImage(this.img);
-        panel.add(this.logo, "cell 0 0 2 2, split 2");
+        panel.add(this.logo, "cell 0 0 3 3, split 2");
         this.nomBoutique = new JLabel(store.getNom());
         panel.add(this.nomBoutique);
         this.add(panel);
@@ -51,21 +52,21 @@ public class DetailStore extends JFrame implements Runnable {
         JButton btn2 = new JButton("Informations sur le produit selectionne");
         JButton btn3 = new JButton("Entrees de stock");
         JButton btn4 = new JButton("Ajouter un produit");
-        panel.add(btn1, "cell 0 2, HEIGHT 40:40:40, WIDTH 150:220:300");
-        panel.add(btn2, "cell 0 3, HEIGHT 40:40:40, WIDTH 150:220:300");
-        panel.add(btn3, "cell 0 4, HEIGHT 40:40:40, WIDTH 150:220:300");
-        panel.add(btn4, "cell 0 5, HEIGHT 40:40:40, WIDTH 150:220:300");
-        /*ProduitDAO pDao = new ProduitDAO();
+        panel.add(btn1, "cell 0 3, HEIGHT 25:25:25, WIDTH 150:220:300");
+        panel.add(btn2, "cell 0 4, HEIGHT 25:25:25, WIDTH 150:220:300");
+        panel.add(btn3, "cell 0 5, HEIGHT 25:25:25, WIDTH 150:220:300");
+        panel.add(btn4, "cell 0 6, HEIGHT 25:25:25, WIDTH 150:220:300");
+        ProduitDAO pDao = new ProduitDAO(Constants.conServ);
         ArrayList<Produit> listProduit = pDao.findFromReference(store.getId());
         MyListModel<Produit> listModel = new MyListModel(listProduit);
         JList<Produit> jlist = new JList(listModel);
         jlist.setFixedCellWidth(90);
         JScrollPane jscrollPane = new JScrollPane(jlist);
-        panel.add(jscrollPane, "cell 2 2 2 4");
+        panel.add(jscrollPane, "cell 2 2 2 5");
         btn2.addActionListener((event) -> {
             DetailProduct ds = new DetailProduct(this.store, jlist.getSelectedValue());
             SwingUtilities.invokeLater(ds);
-        });*/
+        });
     }
 
     public void run() {
