df44e60f431809e40b7962e9f9e83324b5a880f9
Ackincolor <lois.guillet@gmail.com>
2018-03-26 16:16:01 +0200

    release connection

--- Server/src/controller/Main.java
+++ Server/src/controller/Main.java
@@ -19,40 +19,8 @@ public class Main {
     public static void main(String[] args) {
         Database db = null;
         //lancement de l'ecout des connections et création de threads
-        try
-        {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            db = objectMapper.readValue(new File("settings"), Database.class);
-            ConnectionPool listeCo = new ConnectionPool(db);
-
             //creation du server
-            ServerSocket serverSocket = new ServerSocket(Constants.PORT);
-            //liste de tous les clients
-            ArrayList<Client> arraySock = new ArrayList<>();
-
-            int nbConnecté = 0;
-            int i = 0;
-            while(true)
-            {
-                Socket skt = serverSocket.accept();
-                if(skt!=null)
-                {
-                    Client sktClient = new Client(skt,listeCo.getConnectionFromPool());
-                    arraySock.add(sktClient);
-                    nbConnecté+=1;
-                    Thread t = new Thread(sktClient);
-                    t.start();
-                }
-            }
-        }  catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+            Server server = new Server();
+            server.start();
     }
 }
