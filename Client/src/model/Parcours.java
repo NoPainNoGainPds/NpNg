bf900c915fb3801d88b68ec3e1ae9613c7fabb23
Elyass <ebennoukh@gmail.com>
2018-04-29 16:51:42 +0200

    class Parcours v1

--- Client/src/model/Parcours.java
+++ Client/src/model/Parcours.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Parcours {
+    private String nameParcours;
+    private String IdParcours;
+
+
+    public String getName() {
+        return nameParcours;
+    }
+
+    public void setName(String name) {
+        this.nameParcours = nameParcours;
+    }
+
+    public String getId() {
+        return IdParcours;
+    }
+
+    public void setId(String IdParcours) {
+        IdParcours = IdParcours;
+    }
+
+
+    public String toString()
+    {
+        return "nom :"+this.nameParcours;
+    }
 }
